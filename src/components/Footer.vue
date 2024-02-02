1f28762043871154257aa5d5d63b2f8d0c9e3c78
LuizBlume <luiz.fernando.251107@gmail.com>
2024-02-01 21:23:20 -0300

    Hotfix: Footer criado

---
+++ src/components/Footer.vue
@@ -0,0 +1,41 @@
+<template>
+  <footer>
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@200&display=swap"
+      rel="stylesheet"
+    />
+    <div class="footer">
+      <div class="footer-content">
+        <h3 class="h3-footer">
+          <div class="copy">&copy;</div>
+          2024 - Todos os direitos reservados.
+        </h3>
+      </div>
+    </div>
+  </footer>
+</template>
+<style scoped>
+.footer {
+    margin-top: 445px;
+  background-color: #bf6f4e;
+  width: 100vw;
+  height: 7vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.h3-footer {
+  color: #7d7463;
+  font-family: "Kaushan Script";
+  font-style: normal;
+  font-size: 30px;
+  font-weight: 400;
+  line-height: normal;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+}
+.copy {
+  font-family: "Kaushan Script" !important;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Header from '../components/Header.vue'
+import Footer from '../components/Footer.vue'
 </script>
 
 <template>
   <div>
     <Header />
+    <Footer />
   </div>
 </template>
