86f8c4ad610d98ce002b96dcee9eefff5b2199f2
LuizBlume <luiz.fernando.251107@gmail.com>
2024-03-14 15:42:14 -0300

    Hotfix: Alteração de botões e adições de novos itens.

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="row row-cols-1 row-cols-md-3 g-4">
-    <div v-for="(produto, produtoIndex) in dadosProduto" :key="produtoIndex" class="col">
+    <div
+      v-for="(produto, produtoIndex) in dadosProduto"
+      :key="produtoIndex"
+      class="col"
+    >
       <div class="card h-100">
         <img :src="produto.url" class="card-img-top" alt="..." />
         <div class="card-body">
@@ -13,12 +17,31 @@
         </div>
         <div class="card-footer">
           <button class="comprar">
-            <router-link @click="produtoStore.dadosProduto = produto" to="/escolha" class="escolher">Escolher</router-link>
+            <router-link
+              @click="produtoStore.dadosProduto = produto"
+              to="/escolha"
+              class="escolher"
+              >Escolher</router-link
+            >
           </button>
           <button class="comprar escolher esg disp">Disponível</button>
         </div>
         <div class="card-footer">
-          <p>lorem</p>
+            <div class="containerButton">
+            <button
+              class="comprar adicionarItens"
+              @click="incrementarQuantidade(produto)"
+            >
+              +
+            </button>
+            <p class="quantidade-cliente">{{ produto.quantidadeCliente }}</p>
+            <button
+              class="comprar removerItens"
+              @click="decrementarQuantidade(produto)"
+            >
+              -
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -26,27 +49,37 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from "vue"
-import { useProdutoStore } from "../stores/produtoEscolhido"
-import { doc, collection, getDocs } from "firebase/firestore"
-import { db } from "../firebase"
+import { ref, onMounted } from "vue";
+import { useProdutoStore } from "../stores/produtoEscolhido";
+import { doc, collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 
 const dadosProduto = ref([]);
 const produtoStore = useProdutoStore();
 
 onMounted(async () => {
-  let docSnapshot = await getDocs(collection(db, 'produtos'));
+  let docSnapshot = await getDocs(collection(db, "produtos"));
 
   if (docSnapshot) {
     docSnapshot.forEach((produto) => {
-      dadosProduto.value.push({...produto.data(), id: produto.id})
+      dadosProduto.value.push({
+        ...produto.data(),
+        id: produto.id,
+        quantidadeCliente: 1,
+      });
     });
-
-    console.log(dadosProduto.value);
   } else {
     console.log("Nenhum documento encontrado");
   }
-})
+});
+function decrementarQuantidade(produto) {
+  if (produto.quantidadeCliente > 1) {
+    produto.quantidadeCliente -= 1;
+  }
+}
+function incrementarQuantidade(produto) {
+  produto.quantidadeCliente += 1;
+}
 </script>
 
 
@@ -57,6 +90,26 @@ onMounted(async () => {
   text-align: center;
   margin: 0;
 }
+.removerItens {
+  color: #fdfdfd;
+}
+.adicionarItens {
+  color: #fdfdfd;
+}
+.containerButton {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #1a1a1a;
+  border-radius: 5px;
+  width: 100%;
+}
+.quantidade-cliente {
+  color: #fdfdfd;
+  font-size: 14px;
+  padding: 10px;
+  margin: 0;
+}
 .disp {
   background-color: #202020;
   color: #1add1a !important;
@@ -65,9 +118,10 @@ onMounted(async () => {
 }
 .row {
   padding: 10px 20px 10px 20px;
-  background-color: #D5B6A2;
+  background-color: #d5b6a2;
 }
-.g-4, .gx-4 {
+.g-4,
+.gx-4 {
   --bs-gutter-x: 0;
   --bs-gutter-y: 0;
 }
@@ -81,10 +135,10 @@ onMounted(async () => {
   text-align: center;
   margin: 4px 2px;
   cursor: pointer;
-  border-radius: 5px;  
+  border-radius: 5px;
 }
 .escolher {
-  color: #D5B6A2;
+  color: #d5b6a2;
   text-decoration: none;
 }
 .card {
